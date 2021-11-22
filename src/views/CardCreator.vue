b4039cb6bbe80d33a41fbe2e1e43faa9c329c23c
monoglo <monoglo37@gmail.com>
2021-11-22 22:01:31 +0800

    chore(vuetify): add vuetify
    feat(CardCreator): add Card Creator page

---
+++ src/views/CardCreator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="3">left</v-col>
+      <v-col cols="9">right</v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
